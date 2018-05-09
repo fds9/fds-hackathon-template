33fc7c13e72382da9386ef5633ad3312117905f9
koobohyeon <goosee115@gmail.com>
2018-05-09 16:18:31 +0900

    로직 클래스로 수정,게임 초기화 추가

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 // 랜덤으로 숫자를 배열에 넣어준다.
-function randomNumber() {
-  arr = [];
-  for(let i = 0; i < 16; i++) {
-    do {
-      number = Math.floor(Math.random() * 16)
-    } while (arr.indexOf(number) !== -1)
-    arr.push(number)
+
+class Game {
+  puzzle = [];
+  init() {
+    this.puzzle = this.makingArr(this.randomNumber());
   }
-  return arr;
+  randomNumber() {
+    const arr = [];
+    for (let i = 0; i < 16; i++) {
+      do {
+        number = Math.floor(Math.random() * 16);
+      } while (arr.includes(number));
+      arr.push(number);
+    }
+    return arr;
   }
   // 유효한 배열인지 확인한다.
-  function makingArr() {
-    randomNumber();
-  let count = 0;
-  for(let i = 0; i < 16; i++) {
-      for(let j = 0; j < 16-i; j++){
-        if(arr[i] > arr[j]){
-          count++
+  makingArr(arr) {
+    let count = 0;
+    for (let i = 0; i < 16; i++) {
+      for (let j = 0; j < 16 - i; j++) {
+        if (arr[i] > arr[j]) {
+          count++;
         }
       }
+    }
+    if (
+      (count % 2 === 1 &&
+        (0 <= arr.indexOf(0) <= 3 || 8 <= arr.indexOf(0) <= 11)) ||
+      (count % 2 === 0 &&
+        (4 <= arr.indexOf(0) <= 7 || 12 <= arr.indexOf(0) <= 15))
+    ) {
+      const puzzle = [];
+      for (let i = 0; i < 4; i++) {
+        puzzle.push(arr.splice(0, 4));
+      }
+      return puzzle;
+    } else {
+      this.makingArr();
+    }
   }
-  if(((count % 2 === 1 && ( 0 <= arr.indexOf(0) <=3 || 8 <= arr.indexOf(0) <= 11))
-    || (count % 2 === 0 && ( 4 <= arr.indexOf(0) <= 7 || 12 <= arr.indexOf(0) <= 15)))) {
-    return arr;
-  } else {
-    makingArr();
-  }  
-  }
-  makingArr();
-  
+}
+
+const game = new Game();
+game.init();
+console.log(game.puzzle);
+game.init();
+console.log(game.puzzle);
